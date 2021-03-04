663a0db2af19e51985c239670593c943e9f41866
Jayson Larose <jayson@interlaced.org>
2021-03-03 18:59:31 -0800

    * Added pidfile support.

--- pyznap/main.py
+++ pyznap/main.py
@@ -24,6 +24,28 @@ from .send import send_config
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
 CONFIG_DIR = '/etc/pyznap/'
 
+def check_pid(pidfile_path):
+    from sys import argv
+    from os import path, unlink, getpid
+    import psutil
+    
+    if os.path.exists(pidfile_path):
+        # pidfile exists... inspect it for freshness.
+        try:
+            pidno = int(open(pidfile_path, "r").read().strip())
+            try:
+                proc = psutil.Process(pidno)
+                return False
+            except psutil.NoSuchProcess:
+                # pidfile's stale.
+                os.unlink(pidfile_path)
+        except ValueError:
+            # what?
+            os.unlink(pidfile_path)
+    return True
+
+    
+
 def _main():
     """pyznap main function. Parses arguments and calls snap/clean/send functions accordingly.
 
@@ -38,6 +60,8 @@ def _main():
                         dest="verbose", help='print more verbose output')
     parser.add_argument('--config', action="store",
                         dest="config", help='path to config file')
+    parser.add_argument('--pidfile', action="store",
+                        dest="pidfile", default=None, help='path to pid file')
     subparsers = parser.add_subparsers(dest='command')
 
     parser_setup = subparsers.add_parser('setup', help='initial setup')
@@ -86,73 +110,83 @@ def _main():
         sys.exit(1)
     args = parser.parse_args()
 
+
     loglevel = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=loglevel, format='%(asctime)s %(levelname)s: %(message)s',
                         datefmt='%b %d %H:%M:%S', stream=sys.stdout)
     logger = logging.getLogger(__name__)
 
-    logger.info('Starting pyznap...')
+    if args.pidfile is not None:
+        if not check_pid(args.pidfile):
+            logger.info('pidfile {} exists, exiting'.format(args.pidfile))
+            sys.exit(1)
+        open(args.pidfile, "w").write("{}\n".format(os.getpid()))
+    try:
+        logger.info('Starting pyznap...')
 
-    if args.command in ('snap', 'send'):
-        config_path = args.config if args.config else os.path.join(CONFIG_DIR, 'pyznap.conf')
-        config = read_config(config_path)
-        if config == None:
-            return 1
+        if args.command in ('snap', 'send'):
+            config_path = args.config if args.config else os.path.join(CONFIG_DIR, 'pyznap.conf')
+            config = read_config(config_path)
+            if config == None:
+                return 1
 
-    if args.command == 'setup':
-        path = args.path if args.path else CONFIG_DIR
-        create_config(path)
+        if args.command == 'setup':
+            path = args.path if args.path else CONFIG_DIR
+            create_config(path)
 
-    elif args.command == 'snap':
-        # Default if no args are given
-        if not args.take and not args.clean:
-            args.full = True
+        elif args.command == 'snap':
+            # Default if no args are given
+            if not args.take and not args.clean:
+                args.full = True
 
-        if args.take or args.full:
-            take_config(config)
+            if args.take or args.full:
+                take_config(config)
 
-        if args.clean or args.full:
-            clean_config(config)
+            if args.clean or args.full:
+                clean_config(config)
 
-    elif args.command == 'send':
-        if args.source and args.dest:
-            # use args.key if either source or dest is remote
-            source_key, dest_key = None, None
-            if args.dest.startswith('ssh'):
-                dest_key = [args.key] if args.key else None
-            elif args.source.startswith('ssh'):
-                source_key = args.key if args.key else None
-            # if source_key and dest_key are given, overwrite previous value
-            source_key = args.source_key if args.source_key else source_key
-            dest_key = [args.dest_key] if args.dest_key else dest_key
-            # get exclude rules
-            exclude = [args.exclude] if args.exclude else None
-            # check if raw send was requested
-            raw = [args.raw] if args.raw else None
-            # compress ssh zfs send/receive
-            compress = [args.compress] if args.compress else None
-            # use receive resume token
-            resume = [args.resume] if args.resume else None
-            # retry zfs send/receive
-            retries = [args.retries] if args.retries else None
-            # wait interval for retry
-            retry_interval = [args.retry_interval] if args.retry_interval else None
-            # automatically create dest dataset if it does not exist
-            dest_auto_create = [args.dest_auto_create] if args.dest_auto_create else None
+        elif args.command == 'send':
+            if args.source and args.dest:
+                # use args.key if either source or dest is remote
+                source_key, dest_key = None, None
+                if args.dest.startswith('ssh'):
+                    dest_key = [args.key] if args.key else None
+                elif args.source.startswith('ssh'):
+                    source_key = args.key if args.key else None
+                # if source_key and dest_key are given, overwrite previous value
+                source_key = args.source_key if args.source_key else source_key
+                dest_key = [args.dest_key] if args.dest_key else dest_key
+                # get exclude rules
+                exclude = [args.exclude] if args.exclude else None
+                # check if raw send was requested
+                raw = [args.raw] if args.raw else None
+                # compress ssh zfs send/receive
+                compress = [args.compress] if args.compress else None
+                # use receive resume token
+                resume = [args.resume] if args.resume else None
+                # retry zfs send/receive
+                retries = [args.retries] if args.retries else None
+                # wait interval for retry
+                retry_interval = [args.retry_interval] if args.retry_interval else None
+                # automatically create dest dataset if it does not exist
+                dest_auto_create = [args.dest_auto_create] if args.dest_auto_create else None
 
-            send_config([{'name': args.source, 'dest': [args.dest], 'key': source_key,
-                          'dest_keys': dest_key, 'compress': compress, 'exclude': exclude,
-                          'raw_send': raw, 'resume': resume, 'dest_auto_create': dest_auto_create,
-                          'retries': retries, 'retry_interval': retry_interval}])
+                send_config([{'name': args.source, 'dest': [args.dest], 'key': source_key,
+                              'dest_keys': dest_key, 'compress': compress, 'exclude': exclude,
+                              'raw_send': raw, 'resume': resume, 'dest_auto_create': dest_auto_create,
+                              'retries': retries, 'retry_interval': retry_interval}])
 
-        elif args.source and not args.dest:
-            logger.error('Missing dest...')
-        elif args.dest and not args.source:
-            logger.error('Missing source...')
-        else:
-            send_config(config)
+            elif args.source and not args.dest:
+                logger.error('Missing dest...')
+            elif args.dest and not args.source:
+                logger.error('Missing source...')
+            else:
+                send_config(config)
 
-    logger.info('Finished successfully...\n')
+        logger.info('Finished successfully...\n')
+    finally:
+        if args.pidfile is not None:
+            os.unlink(args.pidfile)
     return 0
 
 
